84d0cba1d318f24a1bcd327a5f951cfae2d228e7
Natdenice <clad1987@hotmail.com>
2019-09-15 15:27:49 +0200

    project finished and ready to be published

---
+++ README.md
@@ -0,0 +1,52 @@
+# HPB Price Simulator
+
+
+**HPB Price Simulator** is a project build under Node.js , JavaScript (mainly ES6), HTML5 /CSS3 and Bootstrap 4 framework.
+Particles.js has been implanted to apply a dynamic and modern background made of particles.
+AutoNumeric.js has been added to format correctly numbers inside inputs (thousand separators ...)
+CoinGecko API has been used to get all important datas about HPB.
+
+### High Performance Blockchain Overview
+
+High Performance Blockchain (HPB) is a revolutionary permissionless blockchain architecture that combines a customized hardware Blockchain Offload Engine (BOE), with high-performance blockchain software. The addition of a hardware based BOE, allows for high transaction speed and extremely low latency.
+Despite high-performance, there is no compromise in terms of security and decentralization, as the Node network verifies transactions worldwide and is comprised of a dual election mechanism combining the best aspects of voting and delegation.
+As a permissionless blockchain, anyone can deploy smart contracts, contribute data or use the blockchain. It is our goal to enable and empower as many individuals and enterprises worldwide and build HPB’s DApp ecosystem tailored for enterprise applications.
+
+[Official HPB Website](http://hpb.io)
+
+#### Purposes of HPB Price Simulator App
+
+The main purpose of this application is to provide detailed and constantly updated information to HPB investors.
+This app provides you a lot of essential data, refreshed every second :
+
+- Current Price in USD
+- Current Price in BTC
+- Current MarketCap in USD
+- Current circulating supply
+- USD Value change in 24 Hours
+- BTC Value change in 24 Hours
+- All Time High in BTC
+- All Time High in USD
+- Volume in USD in last 24 Hours
+- Current Market cap Rank
+
+The second purpose of HPB Price Simulator is to simulate the future price of HPB Token.
+The user has the possibility to enter the future expected marketCap of HPB, the future circulating supply
+of HPB and the number of HPB Tokens in his portfolio.
+A result will be displayed at the bottom of the App when the user clicks on "Calculate Price" button.
+
+This personnal project highlights the tremendous financial potential of High Performance Blockchain, the most undervalued chinese project
+in the cryptosphere.
+
+A lot of improvements will be added soon ( like required inputs for example ) and the app will be rebuild under React.js soon.
+
+Thanks to Nikita Vasilev, my coding mentor, for his support [Nikita Vasilev Github](https://github.com/nikitavasilev)
+
+
+
+
+
+
+
+
+
---
+++ crypto.js
@@ -0,0 +1,182 @@
+/* eslint-disable consistent-return */
+/* eslint-disable no-undef */
+/* eslint-disable no-console */
+let portfolioAmount;
+let qntFuturePrice;
+let button;
+
+const marketCapInput = new AutoNumeric('#marketcapInput', {
+  noEventListeners: false,
+  readOnly: false,
+});
+
+const circSupplyInput = new AutoNumeric('#circulatingSupplyInput', {
+  noEventListeners: false,
+  readOnly: false,
+});
+
+const tokenQuantityInput = new AutoNumeric('#tokenquantityInput', {
+  noEventListeners: false,
+  readOnly: false,
+});
+
+const result = document.querySelector('#resultParagraph');
+const coinInfoParagraph = document.querySelector('#cryptoinfo');
+/* eslint-disable no-const-assign */
+
+const formatNumber = (num, rounder) => {
+  // eslint-disable-next-line no-param-reassign
+  num = Math.abs(num);
+  // eslint-disable-next-line no-param-reassign
+  num = num.toFixed(rounder);
+  const numSplit = num.split('.');
+
+  // eslint-disable-next-line prefer-destructuring
+  let int = numSplit[0];
+  if (int.length > 3) {
+    for (let i = 3; i < int.length; i += 4) {
+      int = `${int.substr(0, int.length - i)},${int.substr(int.length - i, i)}`;
+    }
+  }
+  const dec = numSplit[1];
+  return `${int}.${dec}`;
+};
+
+function refresh() {
+  async function getPriceAndMcap() {
+    try {
+      const dataResult = await fetch(
+        'https://api.coingecko.com/api/v3/coins/quant-network',
+      );
+      const data = await dataResult.json();
+
+      // get current price in USD
+      const currentPrice = data.market_data.current_price.usd;
+      // Hardcode current CircSupply due to false circSupply data from API
+      const currentCirculatingSupply = 12072738;
+      // calculate the real mcap with the real circulating supply ( not from API )
+      const currentMarketCap = currentPrice * currentCirculatingSupply;
+      // get current price in BTC
+      const satoshiPrice = data.tickers[4].converted_last.btc;
+      // get Ath In BTC
+      const athInBtc = data.market_data.ath.btc;
+      // get Ath in USD
+      const athInUsd = data.market_data.ath.usd;
+      // get 24H traded volume
+      const volumeIn24H = data.market_data.total_volume.usd;
+      // get valuechange in 24h ( USD )
+      let usdValueChangeIn24H = data.market_data.price_change_percentage_24h_in_currency.usd;
+      // round the percentage of usdValueChangeIn24H
+      usdValueChangeIn24H = usdValueChangeIn24H.toFixed(2);
+      // get valuechange in 24h ( BTC )
+      let btcValueChangeIn24H = data.market_data.price_change_percentage_24h_in_currency.btc;
+      // round the percentage of btcValueChangeIn24H
+      btcValueChangeIn24H = btcValueChangeIn24H.toFixed(2);
+      // get valuechange in 24H ( ETH )
+      let ethValueChangeIn24H = data.market_data.price_change_percentage_24h_in_currency.eth;
+      // round the percentage of ethValueChangeIn24H
+      ethValueChangeIn24H = ethValueChangeIn24H.toFixed(2);
+      // get current mcap rank
+      const marketCapRank = data.market_cap_rank;
+
+      // Display all data
+      const coinInfo = `The current price of Quant Network is $${formatNumber(
+        currentPrice,
+        4,
+      )} ( Ƀ${satoshiPrice.toFixed(8)} ) with a current market cap of $${formatNumber(
+        currentMarketCap,
+        2,
+      )} and a current circulating supply of ${formatNumber(
+        currentCirculatingSupply,
+        2,
+      )} tokens.<br>
+      USD Value change in 24H : ${usdValueChangeIn24H} % <br>
+      BTC Value change in 24H : ${btcValueChangeIn24H} % <br>
+      ETH Value change in 24H : ${ethValueChangeIn24H} % <br>
+      ATH in USD : $${athInUsd} <br>
+      ATH in BTC : Ƀ${formatNumber(athInBtc, 8)} <br>
+      Volume in 24 Hours : $${formatNumber(volumeIn24H, 2)} <br>
+      QNT market cap rank #${marketCapRank}
+      `;
+
+      return coinInfo;
+    } catch (error) {
+      return error;
+    }
+  }
+
+  // eslint-disable-next-line no-shadow
+  getPriceAndMcap().then((result) => {
+    // Cleaning our DOM before refreshing
+    coinInfoParagraph.innerHTML = '';
+    const newContent = `<p class='text-center mt-5' id='cryptoinfo'>${result}</p>`;
+    coinInfoParagraph.insertAdjacentHTML('afterbegin', newContent);
+  });
+}
+
+// Function created to calculate the future value of QNT Price and Porfolio worth
+
+function simulateFuturePrice() {
+  const mc = marketCapInput.getNumber();
+  const cs = circSupplyInput.getNumber();
+  const tq = tokenQuantityInput.getNumber();
+  portfolioAmount = (mc / cs) * tq;
+  qntFuturePrice = mc / cs;
+
+  result.textContent = `
+      Your QNT portfolio will worth $${formatNumber(
+    portfolioAmount,
+    2,
+  )}. With the market cap and the circulating supply specified, QNT coin will worth $${formatNumber(
+  qntFuturePrice,
+  2,
+)}
+    `;
+}
+
+const setupEventListeners = () => {
+  document.addEventListener('keypress', (event) => {
+    if (event.keyCode === 13 || event.which === 13) {
+      simulateFuturePrice();
+      event.preventDefault();
+    }
+  });
+  button = document.querySelector('button');
+  button.addEventListener('click', simulateFuturePrice);
+};
+
+setupEventListeners();
+
+// Create a function to set the circ supply input directly at 42046512
+// ( real circ supply ) and set directly the current mcap (by calling API) in corresponding inputs
+// and let to the user the possibily to modify theses values
+async function circSupplyAndMcapInInputs() {
+  try {
+    const dataResult = await fetch(
+      'https://api.coingecko.com/api/v3/coins/quant-network',
+    );
+    const data = await dataResult.json();
+    // Hardcode current CircSupply due to false circSupply data from API
+    const currentCirculatingSupply = 12072738;
+    circSupplyInput.set(12072738, { readOnly: false });
+
+    // Get the current price of HPB by calling API , we need this information to calculate
+    // the real marketcap ( cause CoinGecko API doesnt have the right circ supply of HPB)
+    const currentPrice = data.market_data.current_price.usd;
+
+    // calculate the real mcap with the real circulating supply ( not from API )
+    const currentMarketCap = currentPrice * currentCirculatingSupply;
+    // Set directly the marketCap into marketCap Input ( and let the user modify this value too)
+    marketCapInput.set(currentMarketCap, { readOnly: false });
+  } catch (error) {
+    return error;
+  }
+}
+
+circSupplyAndMcapInInputs();
+
+// Refresh getPriceAndMcap function with the API Call each 650 ms
+// in order to get fresh live informations in our App
+window.setInterval(() => {
+  refresh();
+}, 650);
